4b6cac1a8e730041c334bb1d5d5770de6bfa58be
wasanx25 <wataru.kikuchi0225@gmail.com>
2018-05-15 02:32:09 +0900

    Add magic card

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,16 @@ fn main() {
              grinder_golem.get_name(),
              grinder_golem.get_level(),
              grinder_golem.get_effect());
+
+    let ritual_sanctuary = Magic {
+        name: "ritual sanctuary".to_string(),
+        spell: MagicSpell::FieldSpell,
+        effect: "Search RitualSpell Magic Card.".to_string()
+    };
+    println!("name: {}, effect: {}, card_type: {}",
+             ritual_sanctuary.get_name(),
+             ritual_sanctuary.get_effect(),
+             ritual_sanctuary.get_spell());
 }
 
 trait Card {
@@ -38,6 +48,49 @@ impl Monster {
 }
 
 impl Card for Monster {
+    fn get_name(&self) -> String {
+        return self.name.to_owned();
+    }
+    fn get_effect(&self) -> String {
+        return self.effect.to_owned();
+    }
+}
+
+struct Magic {
+    name: String,
+    spell: MagicSpell,
+    effect: String,
+}
+
+enum MagicSpell {
+    QuickPlaySpell,
+    FieldSpell,
+    EquipSpell,
+    NormalSpell,
+    RitualSpell,
+    ContinuousSpell,
+}
+
+impl MagicSpell {
+    pub fn get_spell_name(self) -> String {
+        match self {
+            MagicSpell::QuickPlaySpell => "Quick-Play Spell".to_string(),
+            MagicSpell::FieldSpell => "Field Spell".to_string(),
+            MagicSpell::EquipSpell => "Eauip Spell".to_string(),
+            MagicSpell::NormalSpell => "Normal Spell".to_string(),
+            MagicSpell::RitualSpell => "Ritual Spell".to_string(),
+            MagicSpell::ContinuousSpell => "Continuous Spell".to_string(),
+        }
+    }
+}
+
+impl Magic {
+    pub fn get_spell(self) -> String {
+        return self.spell.get_spell_name();
+    }
+}
+
+impl Card for Magic {
     fn get_name(&self) -> String {
         return self.name.to_owned();
     }
