07d822d93be50172420c6c29631dceef785eafe2
wasanx25 <wataru.kikuchi0225@gmail.com>
2018-05-15 02:04:14 +0900

    Modified to getting Monster level method

--- src/main.rs
+++ src/main.rs
@@ -12,7 +12,6 @@ fn main() {
 
 trait Card {
     fn get_name(&self) -> String;
-    fn get_level(&self) -> u8;
     fn get_effect(&self) -> String;
 }
 
@@ -22,12 +21,15 @@ struct Monster {
     effect: String,
 }
 
+impl Monster {
+    pub fn get_level(&self) -> u8 {
+        return self.level.to_owned();
+    }
+}
+
 impl Card for Monster {
     fn get_name(&self) -> String {
         return self.name.to_owned();
-    }
-    fn get_level(&self) -> u8 {
-        return self.level.to_owned();
     }
     fn get_effect(&self) -> String {
         return self.effect.to_owned();
