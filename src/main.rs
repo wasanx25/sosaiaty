bbe25fed437088ee149ef8e8acb1f339e8124aa4
wasanx25 <wataru.kikuchi0225@gmail.com>
2018-05-15 01:55:49 +0900

    Add sample object

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
 fn main() {
-    println!("Hello, world!");
+    let gargoyle = Monster {
+        name: "goagoyle".to_string(),
+        level: 3,
+        effect: "".to_string()
+    };
+    println!("name: {}, level: {}, effect: {effect}",
+             gargoyle.get_name(),
+             gargoyle.get_level(),
+             effect=gargoyle.get_effect());
+}
+
+trait Card {
+    fn get_name(&self) -> String;
+    fn get_level(&self) -> u8;
+    fn get_effect(&self) -> String;
+}
+
+struct Monster {
+    name: String,
+    level: u8,
+    effect: String,
+}
+
+impl Card for Monster {
+    fn get_name(&self) -> String {
+        return self.name.to_owned();
+    }
+    fn get_level(&self) -> u8 {
+        return self.level.to_owned();
+    }
+    fn get_effect(&self) -> String {
+        return self.effect.to_owned();
+    }
 }
